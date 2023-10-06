0a4e19d8e07579911a9896379bcbe3a1d5795c61
andreiasnico <andreiasnico@gmail.com>
2023-10-06 13:34:39 +0300

    jetz schon fertig

--- main/Main.java
+++ main/Main.java
@@ -8,5 +8,8 @@ public class Main {
         int[] noten = new int[] { 10, 20, 30, 39, 47, 48, 60 };
 
         System.out.println(Arrays.toString(notenProzessor.nichtAusreichendeNoten(noten)));
+        System.out.println(notenProzessor.durchschnittWert(noten));
+        System.out.println(Arrays.toString(notenProzessor.abgerundeteNoten(noten)));
+        System.out.println(notenProzessor.maximaleNote(notenProzessor.abgerundeteNoten(noten)));
     }
 }
--- main/NotenProzessor.java
+++ main/NotenProzessor.java
@@ -12,6 +12,7 @@ public class NotenProzessor {
         }
 
 
+
         int[] finaleNoten = new int[0];
 
         for (int i = 0; i < note.length; i++) {
@@ -21,6 +22,34 @@ public class NotenProzessor {
         }
         return finaleNoten;
     }
+    public int durchschnittWert(int[] note){
+        int durchschnittNote = 0;
+        for (int i = 0; i < note.length; i++) {
+            if (note[i] < 0 || note[i] > 100) {
+                System.out.println("Noten soll zwischen 0 und 100 sein.");
+                return -1;
+
+            }
+            durchschnittNote = durchschnittNote + note[i];
+
+        }
+        durchschnittNote = durchschnittNote/note.length;
+        return durchschnittNote;
+    }
+    public int[] abgerundeteNoten(int[] note) {
+        for (int i = 0; i < note.length; i++) {
+            if (note[i] < 0 || note[i] > 100) {
+                System.out.println("Noten soll zwischen 0 und 100 sein.");
+                return null;
+
+            }
+            note[i] = this.round(note[i]);
+
+
+        }
+        return note;
+    }
+
 
     public int round(int note) {
         if (note < 38) {
@@ -33,6 +62,19 @@ public class NotenProzessor {
             }
         }
     }
+    public int maximaleNote(int[] note){
+        int max = 0;
+        for (int i = 0; i < note.length; i++) {
+            if (note[i] < 0 || note[i] > 100) {
+                System.out.println("Noten soll zwischen 0 und 100 sein.");
+                return -1;
+            }
+            if(max < note[i]){
+                max = note[i];
+            }
+        }
+        return max;
+    }
 
     public int[] addToArray(int[] original, int newElement) {
         int[] result = new int[original.length + 1];
@@ -42,5 +84,6 @@ public class NotenProzessor {
         result[original.length] = newElement;
         return result;
     }
+
 
 }
